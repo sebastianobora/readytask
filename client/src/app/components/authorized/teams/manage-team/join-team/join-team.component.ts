d308a38558cb3cef27b0d75346d939d9b6aedb59
sebastianobora <cawbayt@gmail.com>
2021-06-10 22:58:23 +0200

    # Client side: - implemented join-team logic - optimized imports

--- client/src/app/components/authorized/teams/manage-team/join-team/join-team.component.ts
+++ client/src/app/components/authorized/teams/manage-team/join-team/join-team.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {MembershipService} from '../../../../../service/membership.service';
+import {Membership} from '../../../../../entity/membership';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-join-team',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../manage-team-form.css']
 })
 export class JoinTeamComponent implements OnInit {
-
-  constructor() { }
+  code = '';
+  membership: Partial<Membership> = {};
+  constructor(private membershipService: MembershipService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  addMembership(code: string): void{
+    this.membershipService.addByTeamCode(code, this.membership as Membership).subscribe(
+      res => {
+        const url = '/teams/team/' + res.teamId;
+        this.router.navigate([url]);
+      }
+    );
+  }
 }
