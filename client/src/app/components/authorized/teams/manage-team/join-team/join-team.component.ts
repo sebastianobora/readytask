bf03f69d459f37c8070d04641708f37228a8a35e
sebastianobora <cawbayt@gmail.com>
2021-09-13 15:37:00 +0200

    # Client side: - Refactored auth header styles - Refactored auth layout styles - Refactored auth nav styles - Refactored styles in manage team form - Added notifications to join team component

--- client/src/app/components/authorized/teams/manage-team/join-team/join-team.component.ts
+++ client/src/app/components/authorized/teams/manage-team/join-team/join-team.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {MembershipService} from '../../../../../service/membership.service';
-import {Membership} from '../../../../../entity/membership';
 import {Router} from '@angular/router';
+import {NotifierService} from '../../../../../service/notifier.service';
 
 @Component({
   selector: 'app-join-team',
@@ -10,20 +10,23 @@ import {Router} from '@angular/router';
 })
 export class JoinTeamComponent implements OnInit {
   code = '';
-  membership: Partial<Membership> = {};
 
   constructor(private membershipService: MembershipService,
-              private router: Router) {
+              private router: Router,
+              private notifierService: NotifierService) {
   }
 
   ngOnInit(): void {
   }
 
   addMembership(code: string): void {
-    this.membershipService.addByTeamCode(code, this.membership as Membership).subscribe(
+    this.membershipService.addByTeamCode(code).subscribe(
       res => {
         const url = '/teams/team/' + res.teamId;
         this.router.navigate([url]);
+      },
+      () => {
+        this.notifierService.notify('Team with passed code doesn\'t exists!', 'error');
       }
     );
   }
