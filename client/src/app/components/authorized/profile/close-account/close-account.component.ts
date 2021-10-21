e6885d9c2b9c486b83630af62b4f5b9ae4f37ede
sebastianobora <cawbayt@gmail.com>
2021-10-21 18:43:42 +0200

    # Client side: - Refactored close-account validation - Refactored manage-profile submit-button validation

--- client/src/app/components/authorized/profile/close-account/close-account.component.ts
+++ client/src/app/components/authorized/profile/close-account/close-account.component.ts
@@ -3,6 +3,7 @@ import {UserService} from '../../../../service/user.service';
 import {ConfirmationService} from '../../../../service/confirmation.service';
 import {AuthService} from '../../../../security/auth.service';
 import {NotifierService} from '../../../../service/notifier.service';
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-close-account',
@@ -10,26 +11,37 @@ import {NotifierService} from '../../../../service/notifier.service';
   styleUrls: ['./close-account.component.css', '../profile.css']
 })
 export class CloseAccountComponent implements OnInit {
+  closeAccountFormControl: FormControl;
   successfulMessage = 'Your account has been closed.';
   errorMessage = 'Wrong password!';
+  requiredMessage = 'Field is required!';
 
   constructor(private userService: UserService,
               private confirmationService: ConfirmationService,
               private notifierService: NotifierService,
-              private authService: AuthService) {
+              private authService: AuthService,
+              private formBuilder: FormBuilder) {
+    this.closeAccountFormControl = this.getCloseAccountForm();
   }
 
   ngOnInit(): void {
   }
 
-  confirmAndCloseAccount(password: string): void {
-    this.confirmationService.isConfirmed(() => this.closeAccount(password));
+  getCloseAccountForm(): FormControl {
+    return this.formBuilder.control('', [Validators.required]);
   }
 
-  closeAccount(password: string): void {
+  confirmAndCloseAccount(): void {
+    this.confirmationService.isConfirmed(() => this.closeAccount());
+  }
+
+  closeAccount(): void {
+    const password = this.closeAccountFormControl.value;
     this.userService.deleteCurrentLogged(password).subscribe(
       () => this.logoutAndNotify(),
-      () => this.notifierService.notify(this.errorMessage, 'error')
+      () => {
+        this.closeAccountFormControl.setErrors({invalidPassword: true});
+      }
     );
   }
 
