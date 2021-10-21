ef44a1a69c94274f775e8e81d2634d45c8d11c4b
sebastianobora <cawbayt@gmail.com>
2021-10-21 17:32:38 +0200

    # Client side: - Refactored CustomValidators.ts - Added passwordErrorMessages to customValidators class - added method to get error message depends on passed error - Expanded password validation with special char - Implemented validation in edit account component

--- client/src/app/components/authorized/profile/edit-account/edit-account.component.ts
+++ client/src/app/components/authorized/profile/edit-account/edit-account.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../../../service/user.service';
-import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators} from '@angular/forms';
 import {User} from '../../../../entity/user';
 import CustomValidators from '../../../../validators/CustomValidators';
+import {NotifierService} from '../../../../service/notifier.service';
 
 @Component({
   selector: 'app-edit-account',
@@ -12,9 +13,11 @@ import CustomValidators from '../../../../validators/CustomValidators';
 export class EditAccountComponent implements OnInit {
   user!: User;
   changePasswordForm: FormGroup;
+  successfulMessage = 'Your password has been changed!';
 
   constructor(private userService: UserService,
-              private formBuilder: FormBuilder) {
+              private formBuilder: FormBuilder,
+              private notifierService: NotifierService) {
     this.changePasswordForm = this.getChangePasswordForm();
   }
 
@@ -39,8 +42,13 @@ export class EditAccountComponent implements OnInit {
 
   getChangePasswordForm(): FormGroup {
     return this.formBuilder.group({
-      currentPassword: new FormControl('', [Validators.required]),
-      newPassword: new FormControl('', [CustomValidators.Password()]),
+      currentPassword: new FormControl('', Validators.required),
+      newPassword: new FormControl('',
+        [Validators.required,
+          CustomValidators.Password(),
+          Validators.maxLength(30),
+          Validators.minLength(6)]
+      ),
       confirmNewPassword: new FormControl('', Validators.required)
     }, {
       validators: [
@@ -51,6 +59,27 @@ export class EditAccountComponent implements OnInit {
 
   changePassword(): void {
     const f = this.changePasswordForm.controls;
-    this.userService.updatePassword(this.user.id, f.currentPassword.value, f.newPassword.value);
+    this.userService.updatePassword(this.user.id, f.currentPassword.value, f.newPassword.value)
+      .subscribe(
+        () => {
+          this.notifierService.notify(this.successfulMessage, 'success');
+          this.resetChangePasswordForm();
+        },
+        () => {
+          this.currentPassword.reset();
+          this.currentPassword.setErrors({incorrectPassword: true});
+        }
+      );
+  }
+
+  getNewPasswordErrorMessage(error: ValidationErrors): string | null {
+    return CustomValidators.getPasswordErrorMessage(error);
+  }
+
+  resetChangePasswordForm(): void {
+    this.changePasswordForm.reset();
+    Object.keys(this.changePasswordForm.controls).forEach(key => {
+      this.changePasswordForm.controls[key].setErrors(null);
+    });
   }
 }
--- client/src/app/validators/CustomValidators.ts
+++ client/src/app/validators/CustomValidators.ts
@@ -1,6 +1,24 @@
 import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
 
 export default class CustomValidators {
+  private static passwordErrorMessages = {
+    required: 'Password field is required!',
+    minlength: 'Minimum length of password is 5 letters!',
+    maxlength: 'Maximum length of password is 30 letters!',
+    noUpperCase: 'Password must contain at least one capital letter!',
+    noLowerCase: 'Password must contain at least one lowercase letter!',
+    noNumber: 'Password must contain at least one number!',
+    noSpecialChar: 'Password must contain at least one special character!'
+  };
+
+  static getPasswordErrorMessage(error: ValidationErrors): string | null {
+    const errorKey = Object.keys(error)[0] as keyof typeof CustomValidators.passwordErrorMessages;
+    if (!(errorKey in this.passwordErrorMessages)) {
+      return null;
+    }
+    return CustomValidators.passwordErrorMessages[errorKey];
+  }
+
   static Password(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       const controlValue = control.value;
@@ -9,16 +27,21 @@ export default class CustomValidators {
       const hasNumber = /[0-9]+/.test(controlValue);
       const hasLowerCase = /[a-z]+/.test(controlValue);
       const hasUpperCase = /[A-Z]+/.test(controlValue);
-      const isValid = hasNumber && hasLowerCase && hasUpperCase;
+      const hasSpecialChar = /[^A-Za-z0-9]/.test(controlValue);
 
-      if (!hasNumber) {
-        errors = {...errors, noNumber: true};
+      const isValid = hasNumber && hasLowerCase && hasUpperCase && hasSpecialChar;
+
+      if (!hasUpperCase) {
+        errors = {...errors, noUpperCase: true};
       }
       if (!hasLowerCase) {
         errors = {...errors, noLowerCase: true};
       }
-      if (!hasUpperCase) {
-        errors = {...errors, noUpperCase: true};
+      if (!hasNumber) {
+        errors = {...errors, noNumber: true};
+      }
+      if (!hasSpecialChar) {
+        errors = {...errors, noSpecialChar: true};
       }
       return isValid ? null : errors;
     };
