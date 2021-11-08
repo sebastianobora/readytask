0e9b7af21ee683ef9d88e89e63d068eeef574261
sebastianobora <cawbayt@gmail.com>
2021-11-08 16:05:08 +0100

    # Client side: - Added and tested 'marked' library to parse Markdown to HTML syntax

--- client/src/app/components/authorized/tasks/marked-test/marked-test.component.ts
+++ client/src/app/components/authorized/tasks/marked-test/marked-test.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import * as marked from 'marked';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-marked-test',
@@ -6,11 +8,17 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./marked-test.component.css']
 })
 export class MarkedTestComponent implements OnInit {
+  inputText: FormControl = new FormControl('');
 
   constructor() {
+
   }
 
   ngOnInit(): void {
+  }
+
+  parseToMarkdown(text: string): string {
+    return marked.parse(text);
   }
 
 }
