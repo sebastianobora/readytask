300f050a9e016428da45bab9fede8de287062662
sebastianobora <cawbayt@gmail.com>
2021-11-16 01:15:38 +0100

    # Client side: - Refactored auth-nav component - Moved .tooltip class to the global styles.css from copy-to-clipboard component - Created go-back-button component - Refactored styles in participants component

---
+++ client/src/app/components/common/go-back-button/go-back-button.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
+
+@Component({
+  selector: 'app-go-back-button',
+  templateUrl: './go-back-button.component.html',
+  styleUrls: ['./go-back-button.component.css']
+})
+export class GoBackButtonComponent implements OnInit {
+
+  constructor(private location: Location) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
